942d627ae3aba7d30d51f8276af8ebc452f81ae2
GrassR00t <grassr00t@yahoo.com>
2021-06-23 08:10:54 +0000

    [maxipool] local pool server
    Signed-off-by: GrassR00t <grassr00t@yahoo.com>

--- pool/pool_server.py
+++ pool/pool_server.py
@@ -79,7 +79,7 @@ class PoolServer:
         return inner
 
     async def index(self, _) -> web.Response:
-        return web.Response(text="Chia reference pool")
+        return web.Response(text="MaxiDAO testnet pool")
 
     async def get_pool_info(self, _) -> web.Response:
         res: GetPoolInfoResponse = GetPoolInfoResponse(
